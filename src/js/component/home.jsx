7a8a7bec9e50e5bfa224504891af93aa8ba56a4d
Roberto J. Vargas D <rvargas@rv-solutions.net>
2023-08-23 19:59:03 +0000

    Proyecto Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-const Home = () => {
+
+
+const Contador = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="row d-flex justify-content-center align-items-center">
+					<div className="container bg-dark d-flex justify-content-evenly align-items-center rounded">
+						<div className="Logo fa-solid fa-clock border border-3 rounded py-4 px-3 my-2 fs-2 text-white"></div>
+						<div className="annios border border-3 rounded p-4 my-2 fs-2 text-white">{props.annios}</div>
+						<div className="meses border border-3 rounded p-4 my-2 fs-2 text-white">{props.meses}</div>
+						<div className="semanas border border-3 rounded p-4 my-2 fs-2 text-white">{props.semanas}</div>
+						<div className="dias border border-3 rounded p-4 my-2 fs-2 text-white">{props.dias}</div>
+						<div className="horas border border-3 rounded p-4 my-2 fs-2 text-white">{props.horas}</div>
+						<div className="minutos border border-3 rounded p-4 my-2 fs-2 text-white">{props.minutos}</div>
+						<div className="segundos border border-3 rounded p-4 my-2 fs-2 text-white">{props.segundos}</div>
+					</div>
+				</div>
+				<div className="row d-flex justify-content-center align-items-center">
+					<div className="container bg-dark d-flex justify-content-around flex-fill align-items-center rounded">
+						<div className="fa-solid fa-clock text-dark"></div>
+						<div className="fa-solid fa-clock text-dark"></div>
+						<div className="py-3 my-2 fs-6 text-white">AÑOS</div>
+						<div className="py-3 my-2 fs-6 text-white">MESES</div>
+						<div className="py-3 my-2 fs-6 text-white">SEMANAS</div>
+						<div className="py-3 my-2 fs-6 text-white">DIAS</div>
+						<div className="ps-2 py-3 my-2 fs-6 text-white">HORAS</div>
+						<div className="py-3 my-2 fs-6 text-white">MINUTOS</div>
+						<div className="py-3 my-2 fs-6 text-white">SEGUNDOS</div>
+
+					</div>
+				</div>
+
+			</div>
+		</>
+
 	);
 };
 
-export default Home;
+Contador.propTypes = {
+	segundos: PropTypes.number,
+	minutos: PropTypes.number,
+	horas: PropTypes.number,
+	dias: PropTypes.number,
+	semanas: PropTypes.number,
+	meses: PropTypes.number,
+	annios: PropTypes.number
+};
+
+
+export default Contador;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,49 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import Contador from "./component/home.jsx";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+let sec = 0;
+let hour = 0;
+let day = 0;
+let minutes = 0;
+let week = 0;
+let month = 0;
+let year = 0;
 
+setInterval(() => {
+
+  ReactDOM.render(<Contador segundos={sec} minutos={minutes} horas={hour} dias={day} semanas={week} meses={month} annios={year} />, document.querySelector("#app"));
+  sec++;
+  if (sec === 60) {
+    sec = 0;
+    minutes++;
+  }
+  if (minutes === 60) {
+    minutes = 0;
+    hour++;
+  }
+  if (hour === 24) {
+    hour = 0;
+    day++;
+  }
+  if (day === 7) {
+    day = 0;
+    week++;
+  }
+  if (week === 4) {
+    week = 0;
+    month++;
+  }
+  if (month === 12) {
+    month = 0;
+    year++;
+  }
+  
+}, 1000);
+    
+    
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
